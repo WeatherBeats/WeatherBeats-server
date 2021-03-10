5700c1a0f124e0fe10c36d6afebcbe08c0a090a6
Perry Sittser <sittserp@gmail.com>
2021-03-09 16:49:25 -0800

    +night and refactor

--- utils.js
+++ utils.js
@@ -2,74 +2,74 @@ function weatherTranslator(weather) {
 
   const dict = {
     't101d': 'gloomy',
-    't101n': 'gloomy',
+    't101n': 'gloomy+night',
     't102d': 'angry',
-    't102n': 'angry',
+    't102n': 'angry+night',
     't103d': 'angry',
-    't103n': 'angry',
+    't103n': 'angry+night',
     't104d': 'gloomy',
-    't104n': 'gloomy',
+    't104n': 'gloomy+night',
     't105d': 'hard',
-    't105n': 'hard',
+    't105n': 'hard+night',
     'd01d': 'meditation',
-    'd01n': 'meditation',
+    'd01n': 'meditation+night',
     'd02d': 'relaxing',
-    'd02n': 'relaxing',
+    'd02n': 'relaxing+night',
     'd03d': 'mellow',
-    'd03n': 'mellow',
+    'd03n': 'mellow+night',
     'r01d': 'mellow',
-    'r01n': 'mellow',
+    'r01n': 'mellow+night',
     'r02d': 'chill',
-    'r02n': 'chill',
+    'r02n': 'chill+night',
     'r03d': 'gloomy',
-    'r03n': 'gloomy',
+    'r03n': 'gloomy+night',
     'f01d': 'peril',
-    'f01n': 'peril',
+    'f01n': 'peril+night',
     'r04d': 'soothing',
-    'r04n': 'soothing',
+    'r04n': 'soothing+night',
     'r05d': 'soothing',
-    'r05n': 'soothing',
+    'r05n': 'soothing+night',
     'r06d': 'angry',
-    'r06n': 'angry',
+    'r06n': 'angry+night',
     's01d': 'peaceful',
-    's01n': 'peaceful',
+    's01n': 'peaceful+night',
     's02d': 'dreamy',
-    's02n': 'dreamy',
+    's02n': 'dreamy+night',
     's03d': 'blizzard',
-    's03n': 'blizzard',
+    's03n': 'blizzard+night',
     's04d': 'freezing',
-    's04n': 'freezing',
+    's04n': 'freezing+night',
     's05d': 'freezing',
-    's05n': 'freezing',
+    's05n': 'freezing+night',
     's06d': 'alpine',
-    's06n': 'alpine',
+    's06n': 'alpine+night',
     'a01d': 'misty',
-    'a01n': 'misty',
+    'a01n': 'misty+night',
     'a02d': 'smoke',
-    'a02n': 'smoke',
+    'a02n': 'smoke+night',
     'a03d': 'hazy',
-    'a03n': 'hazy',
+    'a03n': 'hazy+night',
     'a04d': 'sandstorm',
-    'a04n': 'sandstorm',
+    'a04n': 'sandstorm+night',
     'a05d': 'foggy',
-    'a05n': 'foggy',
+    'a05n': 'foggy+night',
     'a06d': 'foggy',
-    'a06n': 'foggy',
+    'a06n': 'foggy+night',
     'c01d': 'upbeat',
-    'c01n': 'upbeat',
+    'c01n': 'upbeat+night',
     'c02d': 'happy',
-    'c02n': 'happy',
+    'c02n': 'happy+night',
     'c03d': 'carefree',
-    'c03n': 'carefree',
+    'c03n': 'carefree+night',
     'c04d': 'overcast',
-    'c04n': 'overcast',
-    'u00d': 'rainy tacos',
-    'u00n': 'rainy tacos'
+    'c04n': 'overcast+night',
+    'u00d': 'rainy+tacos',
+    'u00n': 'rainy+tacos+night'
   };
 
-  const searchTerm = Object.getOwnPropertyDescriptor(dict, weather.data[0].weather.icon);
+  const searchTerm = dict[weather.data[0].weather.icon];
 
-  return searchTerm.value;
+  return searchTerm;
 }
 
 function chosenWeatherTranslator(weather) {
@@ -84,9 +84,9 @@ function chosenWeatherTranslator(weather) {
     'hazy': 'hazy'
   };
 
-  const searchTerm = Object.getOwnPropertyDescriptor(dict, weather);
+  const searchTerm = dict[weather];
 
-  return searchTerm.value;
+  return searchTerm;
 }
 
 module.exports = {
